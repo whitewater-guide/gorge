469861cd70c4d27a68bcef9d404c11d5b767192c
doomsower <K.Kuznetcov@gmail.com>
2021-09-24 17:52:56 +0300

    fix: adjust partition_data_proc params

--- storage/migrations/postgres/000003_partitions_2.up.sql
+++ storage/migrations/postgres/000003_partitions_2.up.sql
@@ -1,7 +1,6 @@
 -- Migrate data
 CALL partman.partition_data_proc(
     'public.measurements',
-    p_interval := '1 day',
-    p_batch := 500,
+    p_batch := 100,
     p_source_table := 'public.old_measurements'
 );
