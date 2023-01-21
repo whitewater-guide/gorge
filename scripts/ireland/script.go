84a5ce814448de794915c550e8afd4fd13159a9c
doomsower <K.Kuznetcov@gmail.com>
2023-01-21 23:43:16 +0000

    fix: lint issue

--- scripts/ireland/script.go
+++ scripts/ireland/script.go
@@ -67,7 +67,7 @@ func (s *scriptIreland) Harvest(ctx context.Context, recv chan<- *core.Measureme
 		ref := f.Properties.StationRef
 
 		var val nulltype.NullFloat64
-		json.Unmarshal([]byte(f.Properties.Value), &val)
+		json.Unmarshal([]byte(f.Properties.Value), &val) //nolint:errcheck
 
 		recv <- &core.Measurement{
 			GaugeID: core.GaugeID{
